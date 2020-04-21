84cabff89df3b929fa07b9c2a4d17e6387109522
Rafael <rafael.amtx@gmail.com>
2020-04-21 17:20:24 -0300

    desafio finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,9 @@ function somarContador() {
 
     if (contador == '') {
         document.getElementById('contador').value = 1;
+        document.getElementById('btn-sub').style.fill = corPadraoFundoSvgCounter;
     } else {
-        if (parseInt(contador) === 0) {
+        if ((parseInt(contador) === 0) || (contador === '')) {
             document.getElementById('btn-sub').style.fill = corPadraoFundoSvgCounter;
         }
         document.getElementById('contador').value = parseInt(contador) + 1;
@@ -56,3 +57,80 @@ function alterarCssInputContador(option) {
         document.getElementById('contador').style.border = bordaPadraoInputCounter;
     }
 }
+
+function alterarCssCheckBox(labels) {
+    for (const l of labels) {
+        l.style.color = '#F59393';
+        let [b] = l.getElementsByTagName('span');  
+        b.style.backgroundColor = '#F59393';
+    }
+}
+
+window.onload = function() {
+    document.getElementById('form-stickers').onsubmit= function(e){
+        e.preventDefault();
+        
+        const form = document.getElementById('form-stickers');
+        
+        const tiposStickers = form.getElementsByClassName('checkbox-square');
+        
+        if (!validarTiposStickers(tiposStickers)) {
+            
+            const labels = document.getElementsByClassName('container-checkbox');
+            alterarCssCheckBox(labels);
+
+            alert('Escolha pelo menos um tipo de sticker.');
+            
+            return;
+        }
+        
+        let [contador] = form.getElementsByClassName('input-quantity');
+
+        if (parseInt(contador.value) <= 0 || contador.value == '') {
+            alterarCssInputContador('error');
+            alert('Adicione pelo menos uma unidade de sticker.');
+            return;
+        }
+        
+        let [obs] = form.getElementsByClassName('section-textarea');
+        
+        if (obs.value == '') {
+            alert('Por favor, adicione uma observação.');
+            return;
+        }
+
+        document.getElementById('label-confirm-form').textContent = 'Formulário enviado com sucesso!';
+
+        
+
+        
+
+
+
+    }
+}
+
+function validarTiposStickers(tipos) {
+
+    let aux = 0;
+    for (let t of tipos) {
+        if (t.checked) {
+            aux++;
+        }
+    }
+    
+    return aux > 0 ? true : false;
+}
+
+function validarQuantidadeStickers(contador) {
+
+    let aux = 0;
+    for (let tipo of tipos) {
+        if (tipo.checked) {
+            aux++;
+        }
+    }
+    
+    return aux > 0 ? true : false;
+}
+
